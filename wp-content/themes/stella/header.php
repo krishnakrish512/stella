b0d4391963e15aa2cca21de4674f329250df1222
krishna <krishna@tunatechnology.com>
2022-09-30 12:17:25 +0545

    changes in header

--- wp-content/themes/stella/header.php
+++ wp-content/themes/stella/header.php
@@ -27,26 +27,26 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div class="page-wrapper">
-    <!-- Preloader -->
-    <div class="preloader"></div>
-    <?php $Header = get_field('header_setting', 'option'); ?>
+    <?php wp_body_open(); ?>
+    <div class="page-wrapper">
+        <!-- Preloader -->
+        <div class="preloader"></div>
+        <?php $Header = get_field('header_setting', 'option'); ?>
 
-    <header class="main-header header-style-one">
-        <!--Header Top-->
-        <div class="header-top">
-            <div class="auto-container clearfix">
-                <div class="top-left clearfix">
-                    <div class="text"><span class="icon flaticon-call-answer"></span> Need help? Call Us Now : <a
+        <header class="main-header header-style-one">
+            <!--Header Top-->
+            <div class="header-top">
+                <div class="auto-container clearfix">
+                    <div class="top-left clearfix">
+                        <div class="text"><span class="icon flaticon-call-answer"></span> Need help? Call Us Now : <a
                                 href="tel:<?php echo $Header['phone_number'] ?>"
                                 class="number"><?php echo $Header['phone_number'] ?></a></div>
 
-                </div>
-                <div class="top-right clearfix">
-                    <!-- Info List -->
+                    </div>
+                    <div class="top-right clearfix">
+                        <!-- Info List -->
 
-                    <?php
+                        <?php
                     wp_nav_menu(
                         array(
                             'theme_location' => 'Top-menu',
@@ -55,91 +55,96 @@
                         )
                     );
                     ?>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- End Header Top -->
+            <!-- End Header Top -->
 
-        <!-- Header Upper -->
-        <div class="header-upper">
-            <div class="inner-container">
-                <div class="auto-container clearfix">
-                    <?php
+            <!-- Header Upper -->
+            <div class="header-upper">
+                <div class="inner-container">
+                    <div class="auto-container clearfix">
+                        <?php
                     if (function_exists('the_custom_logo')) {
                         $custom_logo_id = get_theme_mod('custom_logo');
                         $logo = wp_get_attachment_image_url($custom_logo_id, 'full');
 
                     } ?>
-                    <!--Info-->
-                    <div class="logo-outer">
-                        <div class="logo"><a href="<?= get_home_url(); ?>"><img src="<?php echo $logo; ?>" alt=""
-                                                                                title=""></a>
+                        <!--Info-->
+                        <div class="logo-outer">
+                            <div class="logo"><a href="<?= get_home_url(); ?>"><img src="<?php echo $logo; ?>" alt=""
+                                        title=""></a>
+                            </div>
                         </div>
-                    </div>
 
-                    <!--Nav Box-->
-                    <div class="nav-outer clearfix">
-                        <!--Mobile Navigation Toggler For Mobile-->
-                        <div class="mobile-nav-toggler"><span class="icon flaticon-menu-1"></span></div>
+                        <!--Nav Box-->
+                        <div class="nav-outer clearfix">
+                            <!--Mobile Navigation Toggler For Mobile-->
+                            <div class="mobile-nav-toggler"><span class="icon flaticon-menu-1"></span></div>
 
-                        <nav class="main-menu navbar-expand-md navbar-light">
-                            <div class="navbar-header">
-                                <!-- Togg le Button -->
-                                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            <nav class="main-menu navbar-expand-md navbar-light">
+                                <div class="navbar-header">
+                                    <!-- Togg le Button -->
+                                    <button class="navbar-toggler" type="button" data-toggle="collapse"
                                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                         aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="icon flaticon-menu-1"></span>
-                                </button>
-                            </div>
+                                        <span class="icon flaticon-menu-1"></span>
+                                    </button>
+                                </div>
 
-                            <div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
+                                <div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
 
-                                <?php
+                                    <?php
                                 wp_nav_menu(array(
                                     'theme_location' => 'Primary-menu',
                                     'menu_class' => 'navigation clearfix',
                                     'add_li_class' => 'dropdown'
                                 )); ?>
 
-                            </div>
-                        </nav>
-                        <!-- Main Menu End-->
+                                </div>
+                            </nav>
+                            <!-- Main Menu End-->
 
-                        <!-- Outer Box -->
-                        <div class="outer-box clearfix">
-                            <div class="search-box-btn"><span class="icon flaticon-magnifying-glass-1"></span></div>
+                            <!-- Outer Box -->
+                            <div class="outer-box clearfix">
+                                <div class="search-box-btn"><span class="icon flaticon-magnifying-glass-1"></span></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
-        <!--End Header Upper-->
+            <!--End Header Upper-->
 
-        <!-- Mobile Menu  -->
-        <div class="mobile-menu">
-            <div class="menu-backdrop"></div>
-            <div class="close-btn"><span class="icon flaticon-cancel"></span></div>
+            <!-- Mobile Menu  -->
+            <div class="mobile-menu">
+                <div class="menu-backdrop"></div>
+                <div class="close-btn"><span class="icon flaticon-cancel"></span></div>
 
-            <nav class="menu-box">
-                <div class="nav-logo"><a href="<?= get_home_url(); ?>"><img src="<?php echo $logo; ?>" alt=""
-                                                                            title=""></a></div>
-                <ul class="navigation clearfix">
-                    <!--Keep This Empty / Menu will come through Javascript-->
-                </ul>
-                <!--Social Links-->
-                <div class="social-links">
-                    <ul class="clearfix">
-                        <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                        <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
-                        <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                        <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                        <li><a href="#"><span class="fab fa-youtube"></span></a></li>
+                <nav class="menu-box">
+                    <div class="nav-logo"><a href="<?= get_home_url(); ?>"><img src="<?php echo $logo; ?>" alt=""
+                                title=""></a></div>
+                    <ul class="navigation clearfix">
+                        <!--Keep This Empty / Menu will come through Javascript-->
                     </ul>
-                </div>
-            </nav>
-        </div><!-- End Mobile Menu -->
+                    <?php $footer = get_field('footer_setting', 'option'); ?>
+                    <!--Social Links-->
+                    <div class="social-links">
+                        <ul class="clearfix">
+                            <li><a href="<?php echo $footer['twitter_url'] ?>"><span class="fab fa-twitter"></span></a>
+                            </li>
+                            <li><a href="<?php echo $footer['facebook_url'] ?>"><span
+                                        class="fab fa-facebook-square"></span></a></li>
+                            <li><a href="<?php echo $footer['linkedin_url'] ?>"><span
+                                        class="fab fa-linkedin-in"></span></a></li>
+                            <li><a href="<?php echo $footer['google_plus_url'] ?>"><span
+                                        class="fab fa-google-plus-g"></span></a></li>
+                            <!-- <li><a href="#"><span class="fab fa-youtube"></span></a></li> -->
+                        </ul>
+                    </div>
+                </nav>
+            </div><!-- End Mobile Menu -->
 
-    </header>
-    <!-- End Main Header -->
+        </header>
+        <!-- End Main Header -->
